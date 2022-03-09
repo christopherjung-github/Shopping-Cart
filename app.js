f9b28b8364deee47ca7473d917b78dd9e7a88023
Vinh Le <letanvinh0210@gmail.com>
2022-03-08 20:58:07 -0800

    Right now our app.js will render product from product.js to our index.html page

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
-const http = require ('http')
-const fs = require ('fs')
-const port = 3000
+//Select Element
+const productsEl = document.querySelector(".products");
 
-const server = http.createServer(function (request, response){
-    response.writeHead(200,  { 'Content-Type' : 'text/html' })
-    fs.readFile('index.html', function(error, data){
-        if (error) {
-            response.writeHead(404)
-            response.write('Error: File Not Found')
-        }
-        else {
-            response.write(data)
-        }
-        response.end()
+
+//Render Products
+function renderProducts(){
+    products.forEach(function(product){
+        productsEl.innerHTML += 
+            `
+                <div class="product">
+
+                    <!-- image insert here -->
+                    <img src="${product.imgSrc}">
+
+                    <div class="product-info">
+
+                        <h3 class="product-name">${product.name}</h3>
+
+                        <h4 class="product-price"> <small>$</small> ${product.price}</h4>
+
+                        <p class="product-quantity">Quantity: <input value="1" name="">
+
+                        <p class="product-add">
+
+                            <i class="fa fa-trash" aria-hidden="true"></i>
+
+                            <span class="add" onclick = "addItem(${product.id})">Add to Cart</span>
+
+                        </p>
+
+                    </div>
+
+                </div>
+            `
     })
-})
+}
+renderProducts();
+//Cart Array
 
-server.listen(port, function(error){
-    if(error) {
-        console.log('Something went wrong.', error)
-    }
-    else {
-        console.log('Server is listening on port ' + port)
-    }
-})
+//Add to Cart
+var total_price = 0;
+function addItem(id){
+    const item = products.find((product) => product.id === id);
+    total_price += item.price;
+    
+    document.getElementById("total-price").innerHTML = "$ " + total_price; 
+}
