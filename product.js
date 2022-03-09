31980f381b8919413da950e58b790cedebab6e46
Vinh Le <letanvinh0210@gmail.com>
2022-03-08 20:53:06 -0800

    Product.js is a file that contain list of our item, include price name, id and soon Server.js is just what I'm working on to create server for our project

---
+++ product.js
@@ -0,0 +1,26 @@
+const products = [
+    {
+        id : 01,
+        name: "Apple",
+        price: 1,
+        imgSrc : "./image/Apple.jpg",
+    },
+    {
+        id: 02,
+        name: "Orange",
+        price: 1,
+        imgSrc: "./image/Orange.jpg"
+    },
+    {
+        id: 03,
+        name: "Banana",
+        price: 2,
+        imgSrc: "./image/Banana.jpg"
+    },
+    {
+        id: 04,
+        name: "Grape",
+        price: 4,
+        imgSrc: "./image/Grape.jpg"
+    }
+]
