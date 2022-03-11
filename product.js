789d39802918e0dfcd716c6a50d9a6d79914fb71
Carter Loya <cadamloya@gmail.com>
2022-03-11 12:48:08 -0800

    Added images.

--- product.js
+++ product.js
@@ -23,4 +23,41 @@ const products = [
         price: 4,
         imgSrc: "./image/Grape.jpg"
     }
+    {
+        id: 05,
+        name: "Lemon",
+        price: 3,
+        imgSrc: "./image/Lemon.jpg"
+    },
+    {
+        id: 06,
+        name: "Watermelon",
+        price: 5,
+        imgSrc: "./image/Watermelon.jpg"
+    },
+    {
+        id: 07,
+        name: "Pear",
+        price: 3,
+        imgSrc: "./image/Pear.jpg"
+    },
+    {
+        id: 08,
+        name: "Cherry",
+        price: 2,
+        imgSrc: "./image/Cherry.jpg"
+    },
+    {
+        id: 09,
+        name: "Strawberry",
+        price: 3,
+        imgSrc: "./image/Strawberry.jpg"
+    },
+    {
+        id: 10,
+        name: "Nectarine",
+        price: 6,
+        imgSrc: "./image/Nectarine.jpg"
+    }
+]
 ]
