76ed4fd8de2acd1c674db6967f1aba280af3d410
victorvnguyen <v.nguyen@csu.fullerton.edu>
2022-04-05 20:46:54 -0700

    Completed task: "Display quantity for all items in shopping cart"
    Modified addItem() in app.js
    Added renderCart() in app.js
    added cart-items to index.html (line 49)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Select Element
 const productsEl = document.querySelector(".products");
-
+const cartItems = document.querySelector(".cart-items")
 
 //Render Products
 function renderProducts(){
@@ -35,10 +35,11 @@ function renderProducts(){
             `
     })
 }
+
 renderProducts();
 //Cart Array
 let cart = [];
-
+let cartExists = [];
 //Add to Cart
 var total_price = 0;
 var total_item = 0;
@@ -56,7 +57,56 @@ function addItem(id){
 
     document.getElementById("total-price").innerHTML = "$ " + total_price; 
     document.getElementById("total-items").innerHTML = total_item; 
+    
+    for (let i = 0; i < quantity; i++)
+        cart.push(item);
 
-    cart.push(item);
-    console.log(cart);
+    if (!cartExists.includes(item))
+        cartExists.push(item);
+    
+    renderCart();
+
+    var total_quantity = 0;
+
+    //  iterate through the cart to find the total number of specific item
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i] == item) {
+            total_quantity++;
+        }
+    }
+
+    total_cost = total_quantity * item.price;
+
+    document.getElementById("total-price-" + item.id).innerHTML = total_cost;
+    document.getElementById("total-item-" + item.id).innerHTML = total_quantity;
+    //console.log(cart);
 }
+
+function renderCart() {
+    cartExists.forEach(function(item) {
+        var element = document.getElementById("total-item-" + item.id)
+
+        //  if the element already exists, return and do nothing
+        if (typeof(element) != 'undefined' && element != null) {
+            return;
+        }
+        //  add entry if it doesn't exist
+        else {
+            cartItems.innerHTML +=    
+                `
+                    <div class="cart-product-info-${item.id}">
+
+                        <p class="cart-product-name-${item.id}">${item.name}</p>
+
+                        <p class="cart-product-price"> <small>$</small> ${item.price}</p>
+
+                        <p>	<span>Total Item</span>	<span id = "total-item-${item.id}">0</span>	</p>
+                        <p>	<span>Total Price</span>	<span id = "total-price-${item.id}">$0</span>	</p>
+                    </div>
+
+                `
+        }
+    })
+}
+
+
